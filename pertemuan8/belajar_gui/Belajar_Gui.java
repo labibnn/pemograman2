a41edb71f91981b0405dcad7cc317a247b982b3b
labibnn <m.labibniam29@gmail.com>
2025-12-03 12:16:25 +0700

    Add files via upload

---
+++ pertemuan8/belajar_gui/Belajar_Gui.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajar_gui;
+
+/**
+ *
+ * @author MyBook14F
+ */
+public class Belajar_Gui {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
